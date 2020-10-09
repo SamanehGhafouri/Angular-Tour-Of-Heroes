51ba2eba081a6a9299164a0fc50a1e6faac3413a
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-08 23:32:24 -0400

    added dashboard and path to navigate to be the first page after loading local host:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HeroesComponent} from "./heroes/heroes.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
 const routes: Routes = [
   {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+
+  //when app starts navigate is empty and we want it to redirect to dashboard
+  {path: '', redirectTo:'/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({

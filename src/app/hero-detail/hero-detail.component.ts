63d9aec8333816bfddb1f96375bc0df6bf2ec1a5
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-09 00:14:54 -0400

    added path detail for hero detail and extracted the id and wrote a function for that in service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Input} from "@angular/core";
 import {Hero} from "../hero";
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,11 +11,23 @@ import {Hero} from "../hero";
 })
 export class HeroDetailComponent implements OnInit {
   // binding heroes component as a parent of hero detail component(child)
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void{
+    //route.snapshot: is a static image of the route info shortly after the component was created.
+    //paramMap: is a dictionary of route parameter values extracted from the URL
+    //'id': key returns the id of the hero to fetch
+    // +: converts the string to a number, which is what a hero id should be. Route parameters always strings.
+
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,19 +11,13 @@ import {MessageService} from "../message.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HerosComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) { }
 
   getHeroes(): void{
     //Retrieve the heroes from the service
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {

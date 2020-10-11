0184b9b2dcddcda32b14b1453e4271fcd5039a2b
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-10-11 17:10:49 -0400

    added search box to dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -92,4 +92,16 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  /** SEARCH: Get heroes whose name contains search term */
+  serachHeroes(term: string): Observable<Hero[]>{
+    if (!term.trim()){
+      //if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? this.log(`found heroes matching "${term}"`) : this.log(`no heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+        );
+  }
 }
